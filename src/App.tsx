1d9cd21d53ee95dc3203cd306de82985e0c8e164
Rachee <git@rachee.dev>
2025-11-28 04:42:54 +0100

    add startscreen rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useServerSync } from "./lib/hooks/useServerSync";
 import { useServerStore } from "./lib/stores/useServerStore";
+import ClientARScreen from "./screens/ClientARScreen";
 import JoinScreen from "./screens/JoinScreen";
 import LobbyScreen from "./screens/LobbyScreen";
+import StartScreen from "./screens/StartScreen";
 import TapBeatsScreen from "./screens/TapBeatsScreen";
-import ClientARScreen from "./screens/ClientARScreen";
 
 function App() {
 	// 1) React state: we remember the current nickname
@@ -13,6 +14,7 @@ function App() {
 	return (
 		<>
 			{!connected && <JoinScreen />}
+			{connected && currentActivity === "start" && <StartScreen />}
 			{connected && currentActivity === "lobby" && <LobbyScreen />}
 			{connected && currentActivity === "beats" && <TapBeatsScreen />}
 			{connected && currentActivity === "ar" && <ClientARScreen />}
