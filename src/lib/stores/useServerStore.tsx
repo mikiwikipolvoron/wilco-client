40d8d198bba0971f2f8277e8ad84f680a2ba9988
Rachee <git@rachee.dev>
2025-12-08 06:23:12 +0100

    formatting

--- src/lib/stores/useServerStore.tsx
+++ src/lib/stores/useServerStore.tsx
@@ -1,54 +1,56 @@
 import type {
-    ActivityId,
-    GroupDefinitions,
-    Player,
-    ServerState,
+	ActivityId,
+	GroupDefinitions,
+	Player,
+	ServerState,
 } from "@mikiwikipolvoron/wilco-lib/data";
 import { create } from "zustand";
 
 interface ServerStore extends ServerState {
-    currentActivity: ActivityId;
-    players: Record<string, Player>;
-    groups?: GroupDefinitions;
-    connected: boolean;
+	currentActivity: ActivityId;
+	players: Record<string, Player>;
+	groups?: GroupDefinitions;
+	connected: boolean;
 
-    // UI-side state
-    nickname?: string;
+	// UI-side state
+	nickname?: string;
 
-    _handlePlayerJoined: (player: Player) => void;
-    _handlePlayerLeft: (playerId: string) => void;
-    _handleActivityStarted: (activity: ActivityId) => void;
-    _handleGroupsUpdated: (groups: GroupDefinitions) => void;
-    _setConnected: (connected: boolean) => void;
-    setNickname: (nickname: string) => void;
+	_handlePlayerJoined: (player: Player) => void;
+	_handlePlayerLeft: (playerId: string) => void;
+	_handleActivityStarted: (activity: ActivityId) => void;
+	_handleGroupsUpdated: (groups: GroupDefinitions) => void;
+	_setConnected: (connected: boolean) => void;
+	setNickname: (nickname: string) => void;
 }
 
 const initialState = {
-    currentActivity: "lobby" as ActivityId,
-    players: {},
-    groups: undefined,
-    connected: false,
-    lastReaction: undefined,
-    nickname: undefined,
+	currentActivity: "lobby" as ActivityId,
+	players: {},
+	groups: undefined,
+	connected: false,
+	lastReaction: undefined,
+	nickname: undefined,
 };
 
 export const useServerStore = create<ServerStore>((set) => ({
-    ...initialState,
+	...initialState,
 
-    _handlePlayerJoined: (player) => set((state) => ({
-        players: { ...state.players, [player.id]: player },
-    })),
+	_handlePlayerJoined: (player) =>
+		set((state) => ({
+			players: { ...state.players, [player.id]: player },
+		})),
 
-    _handlePlayerLeft: (playerId) => set((state) => {
-        const { [playerId]: _removed, ...players } = state.players;
-        return { players };
-    }),
+	_handlePlayerLeft: (playerId) =>
+		set((state) => {
+			const { [playerId]: _removed, ...players } = state.players;
+			return { players };
+		}),
 
-    _handleActivityStarted: (activity) => set({ currentActivity: activity }),
+	_handleActivityStarted: (activity) => set({ currentActivity: activity }),
 
-    _handleGroupsUpdated: (groups) => set({ groups }),
+	_handleGroupsUpdated: (groups) => set({ groups }),
 
-    _setConnected: (connected) => set({ connected }),
+	_setConnected: (connected) => set({ connected }),
 
-    setNickname: (nickname) => set({ nickname: nickname }),
-}))
+	setNickname: (nickname) => set({ nickname: nickname }),
+}));
